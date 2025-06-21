29c0755482f39c727ad9f3cff8861ea887a03e53
Bella Putri <anamke953@gmail.com>
2025-06-21 09:23:04 +0700

    Create image.js

---
+++ api/image.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res) => {
+    const id = req.query[0];
+    const filePath = path.join('/tmp', id);
+    
+    try {
+        if (fs.existsSync(filePath)) {
+            const imageBuffer = fs.readFileSync(filePath);
+            res.setHeader('Content-Type', 'image/jpeg');
+            return res.send(imageBuffer);
+        } else {
+            return res.status(404).send('Image not found or expired');
+        }
+    } catch (error) {
+        return res.status(500).send('Internal server error');
+    }
+};
